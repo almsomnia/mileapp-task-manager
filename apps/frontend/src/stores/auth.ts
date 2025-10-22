16ccac9a523dd871d167c3381a4b55499c66751c
Riva Almero <rivaalms@proton.me>
2025-10-22 13:20:13 +0700

    fix(app): auth store reactivity

--- apps/frontend/src/stores/auth.ts
+++ apps/frontend/src/stores/auth.ts
@@ -1,34 +1,13 @@
+import { useLocalStorage, StorageSerializers } from "@vueuse/core"
 import { defineStore } from "pinia"
-import { computed } from "vue"
 
 export const useAuthStore = defineStore("auth", () => {
-   const token = computed({
-      get: () => {
-         return localStorage.getItem("auth-token")
-      },
-      set: (value: string | null) => {
-         if (!value) {
-            localStorage.removeItem("auth-token")
-            return
-         }
-         localStorage.setItem("auth-token", value)
-      },
+   const user = useLocalStorage<Model.User | null>("auth-user", null, {
+      deep: true,
+      serializer: StorageSerializers.object
    })
-
-   const user = computed<Model.User | null>({
-      get: () => {
-         const stringified = localStorage.getItem("auth-user")
-         if (!stringified) return stringified
-         return JSON.parse(stringified)
-      },
-      set: (value) => {
-         if (!value) {
-            localStorage.removeItem("auth-user")
-            return
-         }
-         const stringified = JSON.stringify(value)
-         localStorage.setItem("auth-user", stringified)
-      },
+   const token = useLocalStorage<string | null>("auth-token", null, {
+      shallow: true,
    })
 
    return {
