8e45e8a11ec40bd82b78e0fb03582530e0bcae99
Riva Almero <rivaalms@proton.me>
2025-10-22 12:32:52 +0700

    feat(server): wrap auth login response

--- apps/backend/src/modules/auth/auth.controller.ts
+++ apps/backend/src/modules/auth/auth.controller.ts
@@ -1,13 +1,14 @@
 import { Request, Response, NextFunction } from "express"
 import { AuthService } from "./auth.service"
+import { httpResponse } from "@/utils/httpResponse"
 
 export class AuthController {
    private service = new AuthService()
 
    public login = async (req: Request, res: Response, next: NextFunction) => {
       try {
-         const result = await this.service.login(req.body)
-         res.json(result)
+         const { user, token } = await this.service.login(req.body)
+         res.status(200).json(httpResponse(user, { token }))
       } catch (e) {
          next(e)
       }
